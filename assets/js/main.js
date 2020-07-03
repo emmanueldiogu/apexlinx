1e3f0f3727126d4ef9bf54b67163186577845f41
emmanueldiogu <emmanueldiogu@gmail.com>
2020-07-03 07:54:02 +0100

    filter sidebar update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,17 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 3000
     });
-  });
+});
+
+function sidebarOpen() {
+    let sideBar = document.getElementById("contentSidebar")
+    sideBar.classList.add("d-block")
+    let menuBar = document.getElementById("filterMenu")
+    menuBar.classList.add("d-block")
+}
+function sidebarClose() {
+    let sideBar = document.getElementById("contentSidebar")
+    sideBar.classList.remove("d-block")
+    let menuBar = document.getElementById("filterMenu")
+    menuBar.classList.remove("d-block")
+}
